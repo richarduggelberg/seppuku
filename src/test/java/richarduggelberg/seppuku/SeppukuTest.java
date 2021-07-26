bd1644a82827eeb84ea7af8b50b8e69a1f330d2e
richarduggelberg <contact@richarduggelberg.com>
2021-07-26 10:48:22 +0200

    add board test and refactor test names

--- src/test/java/richarduggelberg/seppuku/SeppukuTest.java
+++ src/test/java/richarduggelberg/seppuku/SeppukuTest.java
@@ -19,7 +19,7 @@ public class SeppukuTest
     }
 
     @Test
-    public void testIsCorrectOnSimple4x4Solution()
+    public void isCorrect_validSolution_true()
     {
     	int size = 2;
     	Board b = new Board(2);
@@ -32,5 +32,21 @@ public class SeppukuTest
     	b.setRow(3, row3);
     	b.setRow(4, row4);
         assertTrue(b.isCorrect());
+    }
+
+    @Test
+    public void isCorrect_invalidSolution_false()
+    {
+        int size = 2;
+        Board b = new Board(2);
+        int[] row1 = {1,2,3,4};
+        int[] row2 = {3,4,1,2};
+        int[] row3 = {2,3,4,1};
+        int[] row4 = {4,1,2,3};
+        b.setRow(1, row1);
+        b.setRow(2, row2);
+        b.setRow(3, row3);
+        b.setRow(4, row4);
+        assertFalse(b.isCorrect());
     }
 }
