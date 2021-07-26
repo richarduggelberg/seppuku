414d136ce27d195407eaa30a6c99b40c2da67951
richarduggelberg <contact@richarduggelberg.com>
2021-07-26 10:26:19 +0200

    add failing test for isCorrect

--- src/test/java/richarduggelberg/seppuku/SeppukuTest.java
+++ src/test/java/richarduggelberg/seppuku/SeppukuTest.java
@@ -17,4 +17,20 @@ public class SeppukuTest
     {
         assertTrue( true );
     }
+
+    @Test
+    public void testIsCorrectOnSimple4x4Solution()
+    {
+    	int size = 2;
+    	Board b = new Board(2);
+    	int[] row1 = {1,2,3,4};
+    	int[] row2 = {3,4,1,2};
+    	int[] row3 = {2,3,4,1};
+    	int[] row4 = {4,1,2,3};
+    	b.setRow(1, row1);
+    	b.setRow(2, row2);
+    	b.setRow(3, row3);
+    	b.setRow(4, row4);
+        assertTrue(b.isCorrect());
+    }
 }
