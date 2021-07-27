1663d908fde268170b5f37d5b7508a6be93572f9
richarduggelberg <contact@richarduggelberg.com>
2021-07-27 11:42:38 +0200

    fix compilation error

---
+++ src/main/java/richarduggelberg/seppuku/Solver.java
@@ -0,0 +1,16 @@
+package richarduggelberg.seppuku;
+
+public class Solver {
+	private Board b;
+
+	public Solver(Board b) {
+		this.b = b;
+	}
+
+	public void solve() {
+	}
+
+	public Board getSolved() {
+		return b;
+	}
+}
--- src/test/java/richarduggelberg/seppuku/BoardTest.java
+++ src/test/java/richarduggelberg/seppuku/BoardTest.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class SeppukuTest 
+public class BoardTest 
 {
     /**
      * Rigorous Test :-)
