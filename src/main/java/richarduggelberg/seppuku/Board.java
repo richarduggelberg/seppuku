5463b07f515d172ad0e8c4d9d33910f61b28bf4f
richarduggelberg <contact@richarduggelberg.com>
2021-07-27 10:45:40 +0200

    refactor and more passing tests

--- src/main/java/richarduggelberg/seppuku/Board.java
+++ src/main/java/richarduggelberg/seppuku/Board.java
@@ -32,15 +32,102 @@ public class Board {
 	}
 
 	public boolean boxesCorrect() {
+		int[][] boxes = new int[width][width];
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < width; j++) {
+				int boxRow = (int) Math.floor(i/boxWidth);
+				int boxCol = (int) Math.floor(j/boxWidth);
+				int boxPosi = i%boxWidth;
+				int boxPosj = j%boxWidth;
+				int boxNumber = boxWidth * boxRow + boxCol;
+				int boxPosition = boxWidth * boxPosi + boxPosj;
+				boxes[boxNumber][boxPosition] = board[i][j];
+			}
+		}
+
+		for (int i = 0; i < width; i++) {
+			if (containsError(boxes[i])) {
+				return false;
+			}
+		}
 		return true;
 	}
 
 	public boolean rowsCorrect() {
+		int[][] rows = new int[width][width];
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < width; j++) {
+				rows[i][j] = board[i][j];
+			}
+		}
+
+		for (int i = 0; i < width; i++) {
+			if (containsError(rows[i])) {
+				return false;
+			}
+		}
 		return true;
 	}
 
 	public boolean columnsCorrect() {
+		int[][] columns = new int[width][width];
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < width; j++) {
+				columns[i][j] = board[j][i];
+			}
+		}
+
+		for (int i = 0; i < width; i++) {
+			if (containsError(columns[i])) {
+				return false;
+			}
+		}
 		return true;
+	}
+
+	public boolean containsError(int[] boxRowCol) {
+		if (boxRowCol.length != width) {
+			return true;
+		}
+
+		for (int i = 0; i < width; i++) {
+			if (boxRowCol[i] < 1 || boxRowCol[i] > width) {
+				return true;
+			}
+		}
+
+		for (int i = 0; i < width; i++) {
+			for (int j = i + 1; j < width; j++) {
+				if (boxRowCol[i] == boxRowCol[j]) {
+					return true;
+				}
+			}
+		}
+
+		boolean[] contains = new boolean[width];
+		for (int i = 0; i < width; i++) {
+			contains[i] = false;
+		}
+		for (int i = 0; i < width; i++) {
+			contains[boxRowCol[i]-1] = true;
+		}
+		for (int i = 0; i < width; i++) {
+			if (contains[i] == false) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	public void print() {
+		for (int i = 0; i < width; i++) {
+			String row = "";
+			for (int j = 0; j < width; j++) {
+				row += "" + board[i][j] + " ";
+			}
+			System.out.println(row);
+		}
 	}
 }
 
--- src/test/java/richarduggelberg/seppuku/SeppukuTest.java
+++ src/test/java/richarduggelberg/seppuku/SeppukuTest.java
@@ -36,7 +36,7 @@ public class SeppukuTest
     }
 
     @Test
-    public void isCorrect_invalidSolution_false()
+    public void isCorrect_invalidSolution1_false()
     {
         int size = 4;
         Board b = new Board(4);
@@ -49,5 +49,63 @@ public class SeppukuTest
         b.setRow(3, row3);
         b.setRow(4, row4);
         assertFalse(b.isCorrect());
+    }
+
+    @Test
+    public void isCorrect_invalidSolution2_false()
+    {
+        int size = 4;
+        Board b = new Board(4);
+        int[] row1 = {1,2,3,4};
+        int[] row2 = {3,4,1,2};
+        int[] row3 = {2,3,4,1};
+        int[] row4 = {4,1,2,5};
+        b.setRow(1, row1);
+        b.setRow(2, row2);
+        b.setRow(3, row3);
+        b.setRow(4, row4);
+        assertFalse(b.isCorrect());
+    }
+
+    @Test
+    public void containsError_validSet1_false() {
+        Board b = new Board(4);
+        int[] testSet = {1,2,3,4};
+        assertFalse(b.containsError(testSet));
+    }
+
+    @Test
+    public void containsError_validSet2_false() {
+        Board b = new Board(4);
+        int[] testSet = {4,2,3,1};
+        assertFalse(b.containsError(testSet));
+    }
+
+    @Test
+    public void containsError_containsDuplicate_true() {
+        Board b = new Board(4);
+        int[] testSet = {1,1,2,3};
+        assertTrue(b.containsError(testSet));
+    }
+
+    @Test
+    public void containsError_containsValueTooHigh_true() {
+        Board b = new Board(4);
+        int[] testSet = {1,5,2,4};
+        assertTrue(b.containsError(testSet));
+    }
+
+    @Test
+    public void containsError_containsValueTooLow_true() {
+        Board b = new Board(4);
+        int[] testSet = {1,3,2,0};
+        assertTrue(b.containsError(testSet));
+    }
+
+    @Test
+    public void containsError_setTooBig_true() {
+        Board b = new Board(4);
+        int[] testSet = {1,2,3,4,5};
+        assertTrue(b.containsError(testSet));
     }
 }
