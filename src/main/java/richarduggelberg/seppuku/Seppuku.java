6bfb42c92e5141c8735dfafa605c640d4d0b4d2a
richarduggelberg <contact@richarduggelberg.com>
2021-07-30 15:13:14 +0200

    add javadox for Seppuku.java (main)

--- src/main/java/richarduggelberg/seppuku/Seppuku.java
+++ src/main/java/richarduggelberg/seppuku/Seppuku.java
@@ -1,7 +1,20 @@
 package richarduggelberg.seppuku;
 
+/**
+Main class and host of the main method.
+Currently does nothing as a UI is not implemented.
+This application serves as a base for testing and user functionality is not a priority.
+
+@author richarduggelberg
+@version 1.0
+*/
 public class Seppuku {
+
+	/**
+	Main method of the program.
+	Does nothing at this stage.
+	*/
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+        //Nothing
     }
 }
